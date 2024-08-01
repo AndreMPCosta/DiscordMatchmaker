10db364b779bf92ca13725e52ad368a3452ecd4a
André Costa <andre.miguel.pcosta@gmail.com>
2024-08-01 18:58:08 +0100

    Fixed ready list and move to audio channels

--- bot/main.py
+++ bot/main.py
@@ -18,9 +18,9 @@ class MatchMaker(Client):
 
     async def send_ready_list(self, message):
         output_string = 'Ready to play: \n'
-        for player in enumerate(self.playing_list, start=1):
-            output_string += '{}. {}\n'.format(*player) \
-                if player[0] != len(self.playing_list) else '{}. {}'.format(*player)
+        for index, player in enumerate(self.playing_list, start=1):
+            output_string += '{}. {}\n'.format(index, player[0]) \
+                if player[0] != len(self.playing_list) else '{}. {}'.format(index, player[0])
         await message.channel.send(output_string)
 
     def refresh_players(self):
@@ -63,14 +63,15 @@ class MatchMaker(Client):
                     player = self.players.get(author_id)
                     if not player:
                         await message.channel.send('You are not registered, please register first.')
-                    if player.get('summoner') not in self.playing_list:
+                        return
+                    elif (player.get('summoner'), player.get('tag')) not in self.playing_list:
                         self.playing_list.append((player.get('summoner'), player.get('tag')))
                         self.playing_list_ids[player.get('summoner')] = author_id
                     await self.send_ready_list(message)
             case ["!remove"]:
                 player = self.players.get(author_id)
-                if player.get('summoner') in self.playing_list:
-                    self.playing_list.remove(player.get('summoner'))
+                if (player.get('summoner'), player.get('tag')) in self.playing_list:
+                    self.playing_list.remove((player.get('summoner'), player.get('tag')))
                     self.playing_list_ids.pop(player.get('summoner'))
                     await self.send_ready_list(message)
             case ["!close"]:
@@ -78,18 +79,20 @@ class MatchMaker(Client):
                     await message.channel.send("You don't have enough players to play, you need at least 10")
                 else:
                     output_string = 'Ready to play: \n'
-                    for player in enumerate(self.playing_list, start=1):
-                        output_string += '{}. {}\n'.format(*player)
+                    for index, player in enumerate(self.playing_list, start=1):
+                        output_string += '{}. {}\n'.format(index, player[0])
                     await message.channel.send(output_string)
                     await message.channel.send('Starting the draw! Give me some seconds.')
                     blue_team, red_team, ranks = await balance(self.playing_list)
                     await message.channel.send(beautify_teams(blue_team, red_team, ranks, self.guilds[0].emojis))
                     blue_team_channel = utils.get(self.guilds[0].channels, name='Team 1')
                     red_team_channel = utils.get(self.guilds[0].channels, name='Team 2')
-                    for player in blue_team:
-                        await self.guilds[0].get_member(self.playing_list_ids.get(player)).move_to(blue_team_channel)
-                    for player in red_team:
-                        await self.guilds[0].get_member(self.playing_list_ids.get(player)).move_to(red_team_channel)
+                    for player in blue_team.get('players'):
+                        await self.guilds[0].get_member(int(self.playing_list_ids.get(player))).move_to(
+                            blue_team_channel)
+                    for player in red_team.get('players'):
+                        await self.guilds[0].get_member(int(self.playing_list_ids.get(player))).move_to(
+                            red_team_channel)
 
 
 if __name__ == '__main__':
