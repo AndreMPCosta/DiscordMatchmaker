0680181bee4fed95606ce20c747d2bd55147be0f
André Costa <andre.miguel.pcosta@gmail.com>
2024-08-20 09:26:11 +0100

    Migrated register and reset commands

---
+++ bot/commands/__init__.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+from discord import Message
+
+
+class ClientSingleton:
+    _instance = None
+
+    @classmethod
+    def set_client(cls, client):
+        cls._instance = client
+
+    @classmethod
+    def get_client(cls):
+        if cls._instance is None:
+            raise RuntimeError("Client is not set")
+        return cls._instance
+
+
+@dataclass
+class Command(ABC):
+    name: str
+    description: str
+    usage: str
+    example: str
+
+    @property
+    def client(self):
+        return ClientSingleton.get_client()
+
+    @abstractmethod
+    async def execute(self, message: Message, *args):
+        pass
+
+    async def show_help(self, message: Message):
+        description = self.description
+        usage = self.usage
+        example = f"\n{self.example}" if self.example != self.usage else None
+        _message = f"{description}\n" f"Usage: {usage}{f'Example: {example}' if example else ''}"
+        await message.channel.send(_message)
---
+++ bot/commands/account.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from datetime import datetime
+from logging import getLogger
+
+from discord import Message
+
+from api.models.user import User
+from bot.commands import Command
+
+logger = getLogger(__name__)
+
+
+@dataclass
+class Register(Command):
+    name: str = "register"
+    description: str = "Register a summoner name"
+    usage: str = "!register <summoner>#<tag>"
+    example: str = "!register Faker#EUW"
+
+    async def execute(self, message: Message, *args):
+        updated = False
+        author_id = int(message.author.id)
+        user = await User.get_by_discord_id(author_id)
+        summoner = " ".join(*args)
+        # find the # char
+        if "#" in summoner:
+            summoner, tag = summoner.split("#")
+        else:
+            tag = "EUW"
+        if not user:
+            user = User(discord_id=author_id, summoner=summoner, tag=tag)
+        else:
+            updated = True
+            user.summoner = summoner
+            user.tag = tag
+            user.last_modified = datetime.utcnow()
+        await user.save()
+
+        logger.info(
+            f"Username '{summoner}' and Tag '{tag}' successfully "
+            f"{'registered' if not updated else 'updated'}, tied to {message.author.name}"
+        )
+        await message.channel.send(
+            f"Username '{summoner}' and Tag '{tag}' successfully "
+            f"{'registered' if not updated else 'updated'}, tied to {message.author.name}"
+        )
